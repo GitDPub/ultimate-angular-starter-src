ab39e748d50b71eb04bff2edb4213e2c0046e443
Dieter P <publicpracticas@gmail.com>
2018-07-15 20:08:52 -0400

    notes chapter 2

--- 2-your-first-controller/content/js/MainController.js
+++ 2-your-first-controller/content/js/MainController.js
@@ -1,7 +1,12 @@
 function MainController() {
 
 }
-
+//register with angular
+// getting syntax .module('app')
+// second argument use setter syntax  .module('app', [])
+// better use .controller('MainController', MainController); like a child module
+// child js object returns methods like controller
+// MainController function reference
 angular
   .module('app')
   .controller('MainController', MainController);
