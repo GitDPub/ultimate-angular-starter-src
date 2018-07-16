02a27baa5fa3c449b300731d308a37215d40fb94
Dieter P <publicpracticas@gmail.com>
2018-07-15 20:46:16 -0400

    notes chapter 3

--- 3-bind-$scope/content/js/MainController.js
+++ 3-bind-$scope/content/js/MainController.js
@@ -1,4 +1,11 @@
 function MainController($scope) {
+//bind data from the controller to the view
+// we use $scope object is js object with many options
+//  $scope is public property to make anything attached to it
+//   be available in html
+//  it is necessary to inject it $httpParamSerializerJQLike
+//  function MainController($scope)
+
   $scope.name = 'Todd';
 }
 
